331044bc1da3d486de30623acdc25846c8f4df70
MyatTheingi <myattheingi555@gmail.com>
2023-02-09 17:12:37 +0630

    create the product list

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  products = products;
 
+  share() {
+    window.alert('The product has been shared!')
+  }
 
 }
