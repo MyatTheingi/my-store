d390840e832df1232139be2135c779df113b46a0
MyatTheingi <myattheingi555@gmail.com>
2023-02-10 10:29:14 +0630

    view product details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { Product, products} from '../products';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
+  product : Product | undefined;
+
+  constructor( private route : ActivatedRoute){}
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRouteParams = Number( routeParams.get('productId'));
+    this.product = products.find(product=> product.id === productIdFromRouteParams);
+  }
 
 }
